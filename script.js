dd57507536a1ddd8b025ad8de2956c4df646ba00
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-19 15:36:47 +0600

    pagination  removed

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const images = document.querySelector('.carousel').children;
-const totalImages = images.length;
-let index = 0;
-
-prev.addEventListener('click', () => {
-  nextImage('next');
-})
-
-next.addEventListener('click', () => {
-  nextImage('prev');
-})
-
-function nextImage(direction) {
-  if(direction == 'next') {
-    index++;
-    if(index == totalImages) {
-      index = 0;
-    }
-  } else {
-    if(index == 0) {
-      index = totalImages - 1;
-    } else {
-      index--;
-    }
-  }
-
-  for(let i = 0; i < images.length; i++) {
-    images[i].classList.remove('main');
-  }
-  images[index].classList.add('main');
-}
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
